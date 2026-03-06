28d21dd35e83a124b3fb70565a0f690e9e722443
Charles Rodrigo <charlesrodrio@gmail.com>
2026-03-06 19:00:15 +0000

    update

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -7,24 +7,31 @@ import Rating from "./rating";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader className="p-0 items-center">
-        <Link href={`/product/${product.slug}`}>
-          <Image
-            src={product.images[0]}
-            alt={product.name}
-            height={300}
-            width={300}
-            priority={true}
-          />
+    <Card className="w-full h-full flex flex-col">
+      <CardHeader className="p-0 ">
+        <Link href={`/product/${product.slug}`} className="block">
+          <div className="h-56 w-full flex items-center justify-center overflow-hidden rounded-t-xl bg-white">
+            <Image
+              src={product.images[0]}
+              alt={product.name}
+              height={300}
+              width={300}
+              priority={true}
+            />
+          </div>
         </Link>
       </CardHeader>
-      <CardContent className="p-4 grid gap-4">
-        <div className="text-xs">{product.brand}</div>
+
+      <CardContent className="p-4 flex flex-col gap-2 flex-1">
+        <div className="text-xs text-muted-foreground">{product.brand}</div>
+
         <Link href={`/product/${product.slug}`}>
-          <h2 className="text-sm font-medium">{product.name}</h2>
+          <h2 className="text-sm font-medium leading-snug min-h-[40px] line-clamp-2">
+            {product.name}
+          </h2>
         </Link>
-        <div className="flex-between gap-4">
+
+        <div className="flex items-center justify-between gap-4 mt-auto">
           <Rating value={Number(product.rating)} />
           {product.stock > 0 ? (
             <ProductPrice value={Number(product.price)} />
